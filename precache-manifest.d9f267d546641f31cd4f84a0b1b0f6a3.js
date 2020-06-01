e887dd94d2252b25f0a12adaabb9802cbafd1f6e
ColinJR95 <javcolin1@gmail.com>
2020-06-01 16:55:11 -0700

    Updates

---
+++ precache-manifest.d9f267d546641f31cd4f84a0b1b0f6a3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7227de0e8046798c7b1385b23033a8ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68beb55a5373b5dedde2",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0da73fbc17e603085fd6",
+    "url": "/static/css/main.a515619a.chunk.css"
+  },
+  {
+    "revision": "68beb55a5373b5dedde2",
+    "url": "/static/js/2.81980bc3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.81980bc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0da73fbc17e603085fd6",
+    "url": "/static/js/main.ac136118.chunk.js"
+  },
+  {
+    "revision": "857cfae17f1118125766",
+    "url": "/static/js/runtime-main.e77b7633.js"
+  },
+  {
+    "revision": "489bfa2e1ec0e7d56dbbfee4e2181af9",
+    "url": "/static/media/harryp__-webfont.489bfa2e.woff"
+  },
+  {
+    "revision": "ebc52830844b6e50d7cdcc5f6ef71852",
+    "url": "/static/media/harryp__-webfont.ebc52830.woff2"
+  }
+]);
